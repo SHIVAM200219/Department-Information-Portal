7e8034de0fb2dc9f87968a6635f5ee0a7dbab9e8
SHIVAM200219 <99727460+SHIVAM200219@users.noreply.github.com>
2023-03-10 12:01:36 +0530

    Update queries.php

--- queries.php
+++ queries.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MFSDSAI</title>
+    <title>Courses Department Instructor</title>
     <style>
         .header {
             margin: 10px;
@@ -25,7 +25,7 @@
 
         .content {
             display: flex;
-            justify-content: space-around;
+            justify-content: space-between;
             background-color: aquamarine;
             width: 100%;
             height: 200px;
@@ -93,8 +93,8 @@
     <div class="section">
         <div class="nav">
             <button> <a href="about.php"> About Us</a></button>
-            <button> <a href="registration.php"> Registration</a></button>
-            <button> <a href="login.php">Login</a></button>
+            <!-- <button> <a href="registration.php"> Registration</a></button> -->
+            <button> <a href="index.php">Log Out</a></button>
             <button> <a href="new_associated_faculties.php"> New Entry for faculty</a></button>
             <button> <a href="search_associated_faculties.php"> Search for faculty</a></button>
             <button> <a href="library.php">Library</a></button>
